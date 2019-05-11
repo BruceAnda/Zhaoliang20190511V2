04f197089c64b873315a409277b9e0cb2bdeff02
zhaoliang <2668645098@qq.com>
2019-05-11 10:13:53 +0800

    dev分支开发了新功能

--- app/src/main/java/cn/zhaoliang5156/zhaoliang20190511v2/MainActivity.java
+++ app/src/main/java/cn/zhaoliang5156/zhaoliang20190511v2/MainActivity.java
@@ -3,6 +3,12 @@ package cn.zhaoliang5156.zhaoliang20190511v2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Copyright (C), 2015-2019, 八维集团
+ * Author: zhaoliang
+ * Date: 2019/5/11 10:13 AM
+ * Description:  这里是DEV分支
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         // 需要加一个功能
+
+        // 这里是dev分支
     }
 }
